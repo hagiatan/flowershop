f63660ed0152e12355a866eda4fc17abb6c0623f
hagiatan <giatan15799@gmail.com>
2021-07-18 04:01:03 +0700

    add home about

--- dest/main.js
+++ dest/main.js
@@ -1,1 +1,9 @@
-console.log('Hello CFD');
+// Hamburger
+document
+  .querySelector(".header__hamburger")
+  .addEventListener("click", function () {
+    document.querySelector(".ham1").classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("active");
+    // document.querySelector(".nav").classList.toggle("animate__animated");
+    // document.querySelector(".nav").classList.toggle(" animate__bounce");
+  });
